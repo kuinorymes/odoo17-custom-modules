58f9750ffb7f4eb2face92c6f1d324d215f2b687
Arsen Myroniuk <36411498+kuinorymes@users.noreply.github.com>
2025-10-02 23:31:40 +0300

    Update __manifest__.py

--- crm_lead_products/__manifest__.py
+++ crm_lead_products/__manifest__.py
@@ -14,8 +14,7 @@
         'wizard/crm_lead_add_package_wizard_views.xml'
     ],
     'images': [
-        'static/description/icon.png',
-        'static/description/banner.gif'
+        'static/description/icon.png'
     ],
     'installable': True,
     'application': False,
